90e99b467bbc0cf9d38f58d20e6d73469a3f576e
durangoi21-arch <durangoi21@campusuninunez.edu.co>
2026-02-26 11:46:24 -0500

    ejercicio6.py

--- actividad_condicionales_donacel_urango/ejercicio6.py
+++ actividad_condicionales_donacel_urango/ejercicio6.py
@@ -2,7 +2,7 @@ dia = int(input("Día: "))
 mes = int(input("Mes: "))
 año = int(input("Año: "))
 
-if 1 <= mes <= 12 and 1 <= dia <= 30:
+if año > 0 and 1 <= mes <= 12 and 1 <= dia <= 30:
     print("Fecha correcta")
 else:
     print("Fecha incorrecta")
